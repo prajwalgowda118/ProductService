01e7f0e3128a36d8340092978e7618ddd02e3aa0
prajwalgowda118 <prajwal7683@gmail.comgit config --global user.name prajwalgowda118git config --global user.email prajwal7683@gmail.com>
2024-05-31 16:23:01 +0100

    added dto and hard coded rest calls

--- src/main/java/com/example/demo/DTO/ProductDto.java
+++ src/main/java/com/example/demo/DTO/ProductDto.java
@@ -1,4 +1,17 @@
 package com.example.demo.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ProductDto {
+
+    private Long id;
+    private String title;
+    private String description;
+    private double price;
+    private String image;
+    private String category;
+    private RatingDto rating;
 }
